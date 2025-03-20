f21c96b9c26c7ca02322a36b171e885bae7e66a7
Zachary K. <zkesler2027@gmail.com>
2025-03-20 19:06:31 +0000

    Lots of things Added

---
+++ Characters.py
@@ -0,0 +1,301 @@
+characterZ = {
+  'All Star' : {
+    'Game': 1,
+    'Class' : 'Defence',
+    'Variants' : 10,
+    'Health' : 200
+  },
+  'Scientist' : {
+    'Game': 1,
+    'Class' : 'Support',
+    'Variants' : 10,
+    'Health' : 100
+    
+  },
+  'Foot Soldier' : {
+    'Game': 1,
+    'Class' : 'Attack',
+    'Variants' : 10,
+    'Health' : 150
+    
+  },
+  'Engineer' : {
+    'Game': 1,
+    'Class' : 'Support',
+    'Variants' : 10,
+    'Health' : 125
+  },
+  'Super Brainz' : {
+    'Game': 2,
+    'Class' : 'Attack',
+    'Variants' : 6,
+    'Health' : 200
+  },
+  'Imp' : {
+    'Game': 2,
+    'Class' : 'Attack',
+    'Variants' : 6,
+    'Health' : 75
+  },
+  'Captain Deadbeard' : {
+    'Game': 2,
+    'Class' : 'Defence',
+    'Variants' : 6,
+    'Health' : 100
+  },
+  "80's Action Hero" : {
+    'Game': 3,
+    'Class' : 'Attack',
+    'Variants' : 1,
+    'Health' : 150
+  },
+  'Wizard' : {
+    'Game': 3,
+    'Class' : 'Support',
+    'Variants' : 1,
+    'Health' : 125
+  },
+  'Electric Slide' : {
+    'Game': 3,
+    'Class' : 'Defence',
+    'Variants' : 1,
+    'Health' : 125
+  },
+  'Hover Goat 3000' : {
+    'Game': 2,
+    'Class' : 'Support',
+    'Variants' : 1,
+    'Health' : 125
+  },
+  'TV Head' : {
+    'Game': 3,
+    'Class' : 'Swarm',
+    'Variants' : 1,
+    'Health' : 150
+  }
+}
+
+characterP = {
+    'Peashooter' : {
+        'Game': 1,
+        'Class' : 'Attack',
+        'Variants' : 10,
+        'Health' : 125
+    },
+    'Sunflower' : {
+        'Game': 1,
+        'Class' : 'Support',
+        'Variants' : 10,
+        'Health' : 100
+    },
+    'Chomper' : {
+        'Game': 1,
+        'Class' : 'Attack',
+        'Variants' : 12,
+        'Health' : 150
+    },
+    'Cactis' : {
+        'Game': 1,
+        'Class' : 'Defence',
+        'Variants' : 10,
+        'Health' : 100
+    },
+    'Citron' : {
+        'Game': 2,
+        'Class' : 'Defence',
+        'Variants' : 6,
+        'Health' : 120
+    },
+    'Rose' : {
+        'Game': 2,
+        'Class' : 'Support',
+        'Variants' : 6,
+        'Health' : 110
+    },
+    'Kernal Corn' : {
+        'Game': 2,
+        'Class' : 'Attack',
+        'Variants' : 6,
+        'Health' : 125
+    },
+    'Snapdragon' : {
+        'Game': 3,
+        'Class' : 'Attack',
+        'Variants' : 1,
+        'Health' : 150
+    },
+    'Nightcap' : {
+        'Game': 3,
+        'Class' : 'Attack',
+        'Variants' : 1,
+        'Health' : 75
+    },
+    'Acorn' : {
+        'Game': 3,
+        'Class' : 'Defence',
+        'Variants' : 1,
+        'Health' : 75
+    },
+    'Torchwood' : {
+        'Game': 2,
+        'Class' : 'Attack',
+        'Variants' : 1,
+        'Health' : 200
+    },
+    'Dandelion' : {
+        'Game': 3,
+        'Class' : 'Swarm',
+        'Variants' : 1,
+        'Health' : 150
+    }
+
+}
+
+characters = {
+  'All Star' : {
+    'Game': 1,
+    'Class' : 'Defence',
+    'Variants' : 10,
+    'Health' : 200
+  },
+  'Scientist' : {
+    'Game': 1,
+    'Class' : 'Support',
+    'Variants' : 10,
+    'Health' : 100
+    
+  },
+  'Foot Soldier' : {
+    'Game': 1,
+    'Class' : 'Attack',
+    'Variants' : 10,
+    'Health' : 150
+    
+  },
+  'Engineer' : {
+    'Game': 1,
+    'Class' : 'Support',
+    'Variants' : 10,
+    'Health' : 125
+  },
+  'Super Brainz' : {
+    'Game': 2,
+    'Class' : 'Attack',
+    'Variants' : 6,
+    'Health' : 200
+  },
+  'Imp' : {
+    'Game': 2,
+    'Class' : 'Attack',
+    'Variants' : 6,
+    'Health' : 75
+  },
+  'Captain Deadbeard' : {
+    'Game': 2,
+    'Class' : 'Defence',
+    'Variants' : 6,
+    'Health' : 100
+  },
+  "80's Action Hero" : {
+    'Game': 3,
+    'Class' : 'Attack',
+    'Variants' : 1,
+    'Health' : 150
+  },
+  'Wizard' : {
+    'Game': 3,
+    'Class' : 'Support',
+    'Variants' : 1,
+    'Health' : 125
+  },
+  'Electric Slide' : {
+    'Game': 3,
+    'Class' : 'Defence',
+    'Variants' : 1,
+    'Health' : 125
+  },
+  'Hover Goat 3000' : {
+    'Game': 2,
+    'Class' : 'Support',
+    'Variants' : 1,
+    'Health' : 125
+  },
+  'TV Head' : {
+    'Game': 3,
+    'Class' : 'Swarm',
+    'Variants' : 1,
+    'Health' : 150
+  },
+  'Peashooter' : {
+        'Game': 1,
+        'Class' : 'Attack',
+        'Variants' : 10,
+        'Health' : 125
+    },
+    'Sunflower' : {
+        'Game': 1,
+        'Class' : 'Support',
+        'Variants' : 10,
+        'Health' : 100
+    },
+    'Chomper' : {
+        'Game': 1,
+        'Class' : 'Attack',
+        'Variants' : 12,
+        'Health' : 150
+    },
+    'Cactis' : {
+        'Game': 1,
+        'Class' : 'Defence',
+        'Variants' : 10,
+        'Health' : 100
+    },
+    'Citron' : {
+        'Game': 2,
+        'Class' : 'Defence',
+        'Variants' : 6,
+        'Health' : 120
+    },
+    'Rose' : {
+        'Game': 2,
+        'Class' : 'Support',
+        'Variants' : 6,
+        'Health' : 110
+    },
+    'Kernal Corn' : {
+        'Game': 2,
+        'Class' : 'Attack',
+        'Variants' : 6,
+        'Health' : 125
+    },
+    'Snapdragon' : {
+        'Game': 3,
+        'Class' : 'Attack',
+        'Variants' : 1,
+        'Health' : 150
+    },
+    'Nightcap' : {
+        'Game': 3,
+        'Class' : 'Attack',
+        'Variants' : 1,
+        'Health' : 75
+    },
+    'Acorn' : {
+        'Game': 3,
+        'Class' : 'Defence',
+        'Variants' : 1,
+        'Health' : 75
+    },
+    'Torchwood' : {
+        'Game': 2,
+        'Class' : 'Attack',
+        'Variants' : 1,
+        'Health' : 200
+    },
+    'Dandelion' : {
+        'Game': 3,
+        'Class' : 'Swarm',
+        'Variants' : 1,
+        'Health' : 150
+    }
+}
